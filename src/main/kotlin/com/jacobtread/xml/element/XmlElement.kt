a647b4309722b6a162a39897c22069fad37dcf48
Jacobtread <jacobtread@gmail.com>
2022-07-23 23:01:53 +1200

    Documented XmlElement and XmlIgnorable

--- src/main/kotlin/com/jacobtread/xml/element/XmlElement.kt
+++ src/main/kotlin/com/jacobtread/xml/element/XmlElement.kt
@@ -6,7 +6,15 @@ import com.jacobtread.xml.OutputOptions
  * Represents an element that is a part of a xml tree which
  * can be rendered out onto the final xml representation
  */
-interface XmlElement {
+sealed interface XmlElement {
 
+    /**
+     * Function implemented by each element which handles converting
+     * the xml element into a string representation
+     *
+     * @param builder The string builder to append to
+     * @param indent The current line indentation (none when pretty printing)
+     * @param outputOptions The output settings
+     */
     fun render(builder: Appendable, indent: String, outputOptions: OutputOptions)
 }
--- src/main/kotlin/com/jacobtread/xml/element/XmlIgnorable.kt
+++ src/main/kotlin/com/jacobtread/xml/element/XmlIgnorable.kt
@@ -6,6 +6,13 @@ package com.jacobtread.xml.element
  */
 interface XmlIgnorable {
 
+    /**
+     * Implemented by elements the result of this determines whether
+     * the render for this element will be skipped and whether the
+     * closing tag of the parent can be merged
+     *
+     * @return Whether this element can be ignored
+     */
     fun isIgnorable(): Boolean
 
 }
